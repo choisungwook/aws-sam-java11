744d884d7fbd075dde60d38fd3d656189ea083b1
Administrator <Administrator>
2022-04-19 10:30:54 +0900

    java예제 프로젝트 업로드

---
+++ src/main/java/org/example/Handler.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Map;
+
+public class Handler implements RequestHandler<Map<String,String>, String> {
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    @Override
+    public String handleRequest(Map<String,String> event, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("EVENT: " + gson.toJson(event));
+        
+        return "200 OK";
+    }
+}
